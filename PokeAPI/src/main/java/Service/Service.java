d97cecaf3acb0fb0b50582664a2d629bd3a15761
Vitor Ponce <vitor.ponce20@gmail.com>
2024-08-13 19:56:46 -0300

    added new testing layer

--- PokeAPI/src/main/java/Service/Service.java
+++ PokeAPI/src/main/java/Service/Service.java
@@ -10,29 +10,24 @@ public class Service {
     Endpoint endpoint = new Endpoint();
 
     public void ditto(){
-
         given().baseUri("https://pokeapi.co").
-        when().get(endpoint.ditto()).then().statusCode(200).log().all();
+        when().get(endpoint.ditto()).then().statusCode(200);
     }
-
     public void charmander(){
         given().baseUri("https://pokeapi.co").
-                when().get(endpoint.charmander()).then().statusCode(200).log().all();
+                when().get(endpoint.charmander()).then().statusCode(200);
     }
-
     public void squirtle(){
         given().baseUri("https://pokeapi.co").
-                when().get(endpoint.charmander()).then().statusCode(200).log().all();
+                when().get(endpoint.charmander()).then().statusCode(200);
     }
-
     public void Wartortle (){
         given().baseUri("https://pokeapi.co").
-                when().get(endpoint.charmander()).then().statusCode(200).log().all();
+                when().get(endpoint.charmander()).then().statusCode(200);
     }
-
     public void Blastoise (){
         given().baseUri("https://pokeapi.co").
-                when().get(endpoint.charmander()).then().statusCode(200).log().all();
+                when().get(endpoint.charmander()).then().statusCode(200);
     }
 
 
---
+++ PokeAPI/src/test/java/e2eTest/TestingE2E.java
@@ -0,0 +1,13 @@
+package e2eTest;
+
+import org.testng.annotations.Test;
+
+public class TestingE2E {
+    @Test
+    public void test(){
+        System.out.println("Hello World");
+        System.out.println("Poke Test");
+
+    }
+
+}
